d5e64f2d306cfa00b368a0cac7731a8dfc3bc9ae
Stas Gromov <stgromov@gmail.com>
2022-10-23 23:17:20 +0300

    init: implementing new game

---
+++ script.js
@@ -0,0 +1,66 @@
+const gameField = document.querySelector('.gameField');
+const  empty = {
+    value: 0,
+    top: 0,
+    left: 0
+};
+
+const cells = [];
+cells.push(empty);
+
+function move(index) {
+    const cell = cells[index];
+
+    const leftDiff = Math.abs(empty.left - cell.left);
+    const topDiff = Math.abs(empty.top - cell.top);
+
+    if (leftDiff + topDiff > 1) { return; }
+
+    cell.element.style.left = `${empty.left * 100}px`;
+    cell.element.style.top = `${empty.top * 100}px`;
+
+    const emptyLeft = empty.left;
+    const emptyTop = empty.top;
+    empty.left = cell.left;
+    empty.top = cell.top;
+    cell.left = emptyLeft;
+    cell.top = emptyTop;
+
+    const isFinished = cells.every(cell => {
+        return cell.value === cell.top * 4 + cell.left;
+    });
+
+    if (isFinished) {
+        //alert('You won');
+        setTimeout(() => alert('You won!'), 500);
+    }
+}
+
+const numbers = [...Array(15).keys()]
+.sort(() => Math.random() - 0.5);
+
+for (let i = 1; i <= 15; i++) {
+    const cell = document.createElement('div');
+    const value = numbers[i-1] + 1;
+    cell.className = 'cell';
+    cell.innerHTML = value;
+
+    const left = i % 4;
+    const top = (i-left) / 4;
+
+    cells.push({
+        value: value,
+        left: left,
+        top: top,
+        element: cell
+    }); 
+
+    cell.style.left = `${left * 100}px`;
+    cell.style.top = `${top * 100}px`;
+
+    gameField.append(cell);
+
+    cell.addEventListener('click', () => {
+        move(i);
+    });
+}
